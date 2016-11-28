30cdf29bbbd49011a223d5751b3197c84e822f91
bomber34 <kunstkr1tik@web.de>
2016-11-28 17:07:11 +0100

    Cleaned up MouseMover

--- server/src/de/ovgu/softwareprojekt/servers/mouse/MouseMover.java
+++ server/src/de/ovgu/softwareprojekt/servers/mouse/MouseMover.java
@@ -1,7 +1,5 @@
 package de.ovgu.softwareprojekt.servers.mouse;
 
-import de.ovgu.softwareprojekt.Main;
-
 import java.awt.*;
 import java.awt.event.InputEvent;
 
@@ -11,18 +9,6 @@ import java.awt.event.InputEvent;
  * This class gets the gyroscope data and moves the mouse accordingly
  */
 public class MouseMover extends Mover {
-
-    private float[] filteredData = new float[3];
-    private final int averageSampleSize = 5;
-    private float[][] average = new float[averageSampleSize][3];
-    private int rawCount = 0; //used for average output
-
-    private Point mousePos;
-    //TODO: find the best sensitivity
-    private final float SENSITIVITY = 40f;
-    //TODO: user sensitivity feature in app
-    //allow user to set own sensitivity
-    private float customSensitivity = 0f;
 
     public MouseMover() {
         super();
@@ -44,60 +30,17 @@ public class MouseMover extends Mover {
     /**
      * moves Mouse according to rawData
      *
-     * @param rawData x-,y-,z-Axis of gyroscope
+     * @param data x-,y-,z-Axis of gyroscope
      */
     @Override
-    public void move(float[] rawData) {
+    public void move(float[] data) {
         PointerInfo pi = MouseInfo.getPointerInfo();
-        mousePos = pi.getLocation();
+        Point mousePos = pi.getLocation();
 
-        float[] axesValues = filter(rawData);
-
-        int xAxis = (int) axesValues[ZAXIS];   //Up down movement on screen is achieved my rotating phone by z-axis
-        int yAxis = (int) axesValues[XAXIS];
+        int xAxis = (int) data[ZAXIS];   //Up down movement on screen is achieved my rotating phone by z-axis
+        int yAxis = (int) data[XAXIS];
 
         moveBot.mouseMove(mousePos.x - xAxis, mousePos.y - yAxis);
-    }
-
-    /**
-     * takes rawData[] and turns it into useful data by applying the average of the last
-     * last n gyroscope values. n = averageSampleSize
-     *
-     * @param rawData unfiltered gyroscope data
-     * @return a float array which has useful axes values
-     */
-
-    //TODO: implement method
-    public float[] filter(float[] rawData) {
-        //TODO: filter value should be customizable
-        rawData[XAXIS] *= (SENSITIVITY + customSensitivity);
-        rawData[YAXIS] *= (SENSITIVITY + customSensitivity);
-        rawData[ZAXIS] *= (SENSITIVITY + customSensitivity);
-
-        //enter new gyroscope values
-        average[rawCount][0] = rawData[XAXIS];
-        average[rawCount][1] = rawData[YAXIS];
-        average[rawCount][2] = rawData[ZAXIS];
-
-        //create average of all values in the sample size
-        for (int i = 0; i < averageSampleSize; i++) {
-            filteredData[XAXIS] += average[rawCount][XAXIS];
-            filteredData[YAXIS] += average[rawCount][YAXIS];
-            filteredData[ZAXIS] += average[rawCount][ZAXIS];
-        }
-        rawCount++;
-
-        //rawCount rotates through the array
-        if (rawCount == averageSampleSize) {
-            rawCount = 0;
-        }
-
-        //calculate average
-        for (int i = 0; i < filteredData.length; i++) {
-            filteredData[i] /= averageSampleSize;
-        }
-
-        return filteredData;
     }
 
     /**
