f7c36566583dddb80124e740d27651af31772d24
meelph <ThousandPies@web.de>
2016-11-28 18:29:03 +0100

    working on options sharedpreferences to implement next

--- app/app-module/src/main/java/de/ovgu/softwareprojektapp/OptionsActivity.java
+++ app/app-module/src/main/java/de/ovgu/softwareprojektapp/OptionsActivity.java
@@ -1,11 +1,14 @@
 package de.ovgu.softwareprojektapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import de.ovgu.softwareprojekt.SensorType;
 
@@ -15,8 +18,7 @@ public class OptionsActivity extends AppCompatActivity {
     Button mBackBtn;
     int mNumberOfSensors;
 
-    //TextView[] mSensorNames = new TextView[5];
-
+    //public static final String PREFS_NAME = "SensitivitySettings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +28,27 @@ public class OptionsActivity extends AppCompatActivity {
         mSensorOptions = (LinearLayout) findViewById(R.id.SensorOptions);
         mBackBtn = (Button) findViewById(R.id.backFromOptionsBtn);
 
-        mNumberOfSensors =  SensorType.values().length-1;
+        mNumberOfSensors =  SensorType.values().length;
 
         createSensorOptions();
+
+        //SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+
     }
     public void createSensorOptions(){
         for (int i = 0; i < mNumberOfSensors; i++){
-           // mSensorNames[i] = new TextView(OptionsActivity.this);
-            //mSensorNames[i].setText(SensorType.values()[i].toString());
 
-            TextView sensorName1 = new TextView(OptionsActivity.this);
-            sensorName1.setText(SensorType.values()[i].toString());
-            sensorName1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, mSensorOptions.getHeight() / (2*mNumberOfSensors)));
+            TextView text = new TextView(OptionsActivity.this);
+            text.setText(SensorType.values()[i].toString());
+            text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
-            SeekBar seek1 = new SeekBar(OptionsActivity.this);
-            seek1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, mSensorOptions.getHeight() / (2*mNumberOfSensors)));
+            SeekBar seek = new SeekBar(OptionsActivity.this);
+            seek.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            seek.setProgress(50);
 
+
+            mSensorOptions.addView(text);
+            mSensorOptions.addView(seek);
 
 
         }
