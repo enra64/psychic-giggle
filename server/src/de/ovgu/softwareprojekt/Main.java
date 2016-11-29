449363dbb70de9aca5c060f7c91cf5400cad4e40
bomber34 <kunstkr1tik@web.de>
2016-11-29 20:09:36 +0100

    I am the best shit coder aka main needs to throw AWTException, too

--- server/src/de/ovgu/softwareprojekt/Main.java
+++ server/src/de/ovgu/softwareprojekt/Main.java
@@ -2,11 +2,12 @@ package de.ovgu.softwareprojekt;
 
 import de.ovgu.softwareprojekt.servers.mouse.MouseServer;
 
+import java.awt.*;
 import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, AWTException {
         new MouseServer(null).start();
     }
 }
