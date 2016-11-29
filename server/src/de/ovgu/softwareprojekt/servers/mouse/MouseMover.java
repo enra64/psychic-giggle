2f3b0f58c5b3b35288b4a809e079b3cf40bd34d7
bomber34 <kunstkr1tik@web.de>
2016-11-29 19:52:27 +0100

    Removed abstract Mover class Cleaned up some comments, TODOs

--- server/src/de/ovgu/softwareprojekt/servers/mouse/MouseMover.java
+++ server/src/de/ovgu/softwareprojekt/servers/mouse/MouseMover.java
@@ -1,5 +1,8 @@
 package de.ovgu.softwareprojekt.servers.mouse;
 
+import de.ovgu.softwareprojekt.DataSink;
+import de.ovgu.softwareprojekt.SensorData;
+
 import java.awt.*;
 import java.awt.event.InputEvent;
 
@@ -8,18 +11,34 @@ import java.awt.event.InputEvent;
  * <p>
  * This class gets the gyroscope data and moves the mouse accordingly
  */
-public class MouseMover extends Mover {
+public class MouseMover implements DataSink {
 
-    public MouseMover() {
-        super();
+    /**
+     * is responsible for emulating hardware input
+     */
+    private Robot mMoveBot;
+
+    /**
+     * Constants which describe which index of sensorData.data is responsible for which axis
+     */
+    private final int XAXIS = 0, YAXIS = 1, ZAXIS = 2;
+
+    /**
+     * A class which is responsible for mouse behaviour
+     * @throws AWTException is thrown if low level input is not allowed on device
+     */
+    public MouseMover() throws AWTException {
+
+        mMoveBot = new Robot();
+
         resetPosToCenter();
     }
 
     /**
      * Set Mouse Cursor Position to monitor position
-     * //TODO:Test with more than one monitor
-     * This method may break on devices which use more than one monitor?
      */
+
+    //TODO:Test with more than one monitor, may glitch on multimonitors
     public void resetPosToCenter() {
         //get mouse location
         Point point = MouseInfo.getPointerInfo().getLocation();
@@ -40,14 +59,14 @@ public class MouseMover extends Mover {
 
                 if(gcBounds.contains(point)) {
                     displayBounds=gcBounds;
-                    moveBot.mouseMove(devices[i].getDisplayMode().getWidth() / 2, devices[i].getDisplayMode().getHeight() / 2);
+                    mMoveBot.mouseMove(devices[i].getDisplayMode().getWidth() / 2, devices[i].getDisplayMode().getHeight() / 2);
                 }
             }
         }
         //not found, get the bounds for the default display
         if(displayBounds == null) {
             GraphicsDevice device = e.getDefaultScreenDevice();
-            moveBot.mouseMove(device.getDisplayMode().getWidth() / 2, device.getDisplayMode().getHeight() / 2);
+            mMoveBot.mouseMove(device.getDisplayMode().getWidth() / 2, device.getDisplayMode().getHeight() / 2);
 
         }
     }
@@ -57,7 +76,6 @@ public class MouseMover extends Mover {
      *
      * @param data x-,y-,z-Axis of gyroscope
      */
-    @Override
     public void move(float[] data) {
         PointerInfo pi = MouseInfo.getPointerInfo();
         Point mousePos = pi.getLocation();
@@ -65,27 +83,37 @@ public class MouseMover extends Mover {
         int xAxis = (int) data[ZAXIS];   //Up down movement on screen is achieved my rotating phone by z-axis
         int yAxis = (int) data[XAXIS];
 
-        moveBot.mouseMove(mousePos.x - xAxis, mousePos.y - yAxis);
+        mMoveBot.mouseMove(mousePos.x - xAxis, mousePos.y - yAxis);
     }
 
     /**
-     * This Method recieves a boolean and if true presses the left mouseButton and releases the button again if false
+     * This Method receives a boolean and if true presses the left mouseButton and releases the button again if false
      *
      * @param isClicked
      */
     public void click(int buttonID, boolean isClicked) {
         if (buttonID == MouseServer.LEFT_MOUSE_BUTTON) {
             if (isClicked)
-                moveBot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                mMoveBot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
             else {
-                moveBot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                mMoveBot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
             }
         } else if (buttonID == MouseServer.RIGHT_MOUSE_BUTTON) {
             if (isClicked) {
-                moveBot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+                mMoveBot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
             } else
-                moveBot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+                mMoveBot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
         }
+
+    }
+
+    @Override
+    public void onData(SensorData sensorData) {
+        move(sensorData.data);
+    }
+
+    @Override
+    public void close() {
 
     }
 }
