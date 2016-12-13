3a049fe3fc4fb1ae5289f9e00cf5346fa8596adf
enra64 <sujomear@arcor.de>
2016-12-13 15:39:29 +0100

    changed main to accept a parameter dictating the server to be started

--- server/src/de/ovgu/softwareprojekt/Main.java
+++ server/src/de/ovgu/softwareprojekt/Main.java
@@ -1,6 +1,7 @@
 package de.ovgu.softwareprojekt;
 
 import de.ovgu.softwareprojekt.servers.mouse.MouseServer;
+import de.ovgu.softwareprojekt.servers.nes.NesServer;
 
 import java.awt.*;
 import java.io.IOException;
@@ -8,6 +9,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, AWTException {
-        new MouseServer(null).start();
+        switch(args[0]){
+            case "mouse":
+                new MouseServer(null).start();
+                break;
+            default:
+            case "nes":
+                new NesServer(null).start();
+                break;
+        }
     }
 }
