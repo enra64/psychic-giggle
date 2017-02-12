b102a6d202840347f3ef1d7c895722ac938e2989
enra64 <sujomear@arcor.de>
2017-02-12 21:45:39 +0100

    added copyright notice to coppelia .java files

--- abgabefolder/psychic_server/src/coppelia/StringW.java
+++ abgabefolder/psychic_server/src/coppelia/StringW.java
@@ -1,4 +1,30 @@
-// PUT_VREP_REMOTEAPI_COPYRIGHT_NOTICE_HERE
+// This file is part of the REMOTE API
+// 
+// Copyright 2006-2016 Coppelia Robotics GmbH. All rights reserved. 
+// marc@coppeliarobotics.com
+// www.coppeliarobotics.com
+// 
+// The REMOTE API is licensed under the terms of GNU GPL:
+// 
+// -------------------------------------------------------------------
+// The REMOTE API is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// THE REMOTE API IS DISTRIBUTED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+// WARRANTY. THE USER WILL USE IT AT HIS/HER OWN RISK. THE ORIGINAL
+// AUTHORS AND COPPELIA ROBOTICS GMBH WILL NOT BE LIABLE FOR DATA LOSS,
+// DAMAGES, LOSS OF PROFITS OR ANY OTHER KIND OF LOSS WHILE USING OR
+// MISUSING THIS SOFTWARE.
+// 
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with the REMOTE API.  If not, see <http://www.gnu.org/licenses/>.
+// -------------------------------------------------------------------
+//
+// This file was automatically created for V-REP release V3.3.2 on August 29th 2016
 
 package coppelia;
 
